ad84f3609b0f23db8994095fae50fde2c2b74dca
Ethan Joseph <EthanJoseph@Macintosh.local>
2016-06-20 16:05:04 -0400

    Creates the Book class with private fields and setters/getters

---
+++ src/Book.java
@@ -0,0 +1,55 @@
+import java.time.LocalDate;
+
+public class Book {
+	//immutable fields
+	private String title;
+	private String author;
+	private int subject;
+	
+	//settable fields
+	private String status;
+	private LocalDate due;
+	private int borrowID;
+	
+	//constructor
+	public Book(String title, String author, int subject, String status){
+		this.title = title;
+		this.author = author;
+		this.subject = subject;
+		this.status = status;
+	}
+	
+	
+	//getter methods
+	public String getTitle() {
+		return title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public int getSubject() {
+		return subject;
+	}
+	public LocalDate getDue() {
+		return due;
+	}
+	public int getBorrowID() {
+		return borrowID;
+	}
+	
+	//setter methods
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	public void setDue(LocalDate due) {
+		this.due = due;
+	}
+	public void setBorrowID(int borrowID) {
+		this.borrowID = borrowID;
+	}
+	
+}
